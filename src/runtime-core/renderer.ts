49da02e4ca927eb3864e7daa9807af8a22ba8ada
smxjj1 <13027612953@163.com>
2022-04-25 20:26:49 +0800

    fixed:实现初始化 component 主流程

---
+++ src/runtime-core/renderer.ts
@@ -0,0 +1,24 @@
+import { createComponentInstance, setupComponent } from "./component";
+
+export function render(vnode, container) {
+
+  patch(vnode, container);
+}
+function patch(vnode: any, container: any) {
+    processComponent(vnode, container);
+}
+
+function processComponent(vnode: any, container: any) {
+    mountComponent(vnode, container);
+}
+
+function mountComponent(vnode: any, container: any) {
+    const instance = createComponentInstance(vnode);
+    setupComponent(instance);
+    setupRenderEffect(instance, container);
+  }
+  function setupRenderEffect(instance: any, container) {
+    const subTree = instance.render();
+  
+    patch(subTree, container);
+  }
